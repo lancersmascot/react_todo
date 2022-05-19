b505c69c2e245d11e205ebc734dcf2dd0ef59f17
jibulya0920 <webdev980101@gmail.com>
2022-05-20 00:23:40 +0900

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count : 4
+    }
+  }
+
+  isEven() {
+    return this.state.count % 2 === 0 ? "Even" : "Odd"
+  }
+
+  render = () =>
+    <h4 className="bg-primary text-white text-center p-2 m-1">
+      Number of things: {this.isEven()}
+    </h4>
 }
-
-export default App;
