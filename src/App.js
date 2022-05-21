85ae7c81789cbbf7dc8e1b4b6cb9e697a87faced
jibulya0920 <webdev980101@gmail.com>
2022-05-21 15:58:08 +0900

    complete increasing one by one

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      count : 4
+      count :3
     }
   }
 
-  isEven() {
-    return this.state.count % 2 === 0 ? "Even" : "Odd"
+  isEven(val) {
+    return val % 2 === 0 ? "Even" : "Odd"
   }
 
+  getClassName(val){
+    return val % 2 === 0 ? "bg-primary text-white text-center p-2 m-1" 
+                         : "bg-secondary text-white text-center p-2 m-1"
+  }
+
+  handleClick = () => this.setState({count: this.state.count + 1})
+
   render = () =>
-    <h4 className="bg-primary text-white text-center p-2 m-1">
-      Number of things: {this.isEven()}
+    <h4 className={this.getClassName(this.state.count)}>
+      Number of things: {this.isEven(this.state.count)}
     </h4>
 }
